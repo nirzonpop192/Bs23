7b6b490435816823e85df5ebd1032e8c63772107
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2022-12-28 23:59:39 +0600

    add repository

--- app/src/main/java/com/example/bs23/repository/HomeRepository.kt
+++ app/src/main/java/com/example/bs23/repository/HomeRepository.kt
@@ -1,4 +1,17 @@
 package com.example.bs23.repository
 
-class HomeRepository {
+import com.example.bs23.data.model.GitHubResponse
+import com.example.bs23.data.remote.GitHubApi
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class HomeRepository @Inject constructor( private val api:GitHubApi) {
+
+    suspend fun fetchRepositoryApi(query: String , sort: String, order: String, per_page: Int,
+        page: Int ): GitHubResponse {
+        return api.getRepository(query , sort, order, per_page, page)
+    }
+
 }
